59209fb0782e4220ae2891a7b8c691431c28779d
mateus <teuse853@gmail.com>
2024-12-21 21:42:58 -0300

    Primeiro commit

---
+++ ornizacao-financeira/src/main/java/com/ornizacao_financeira/ornizacao_financeira/OrnizacaoFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.ornizacao_financeira.ornizacao_financeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrnizacaoFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrnizacaoFinanceiraApplication.class, args);
+	}
+
+}
---
+++ ornizacao-financeira/src/test/java/com/ornizacao_financeira/ornizacao_financeira/OrnizacaoFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ornizacao_financeira.ornizacao_financeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrnizacaoFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
